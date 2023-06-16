d3d85fdd432989bde15c1d8b0e7dabfcd6f5f8b6
SafaaC <safaaacherak@gmail.com>
2023-06-16 15:07:36 +0530

    update method

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +43,13 @@ public class BookController {
     @DeleteMapping("/books/{bookId}")
     public void deleteBook(@PathVariable("bookId") int bookId){
             bookService.deleteBook(bookId);
+    }
+
+    //update book handler
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@RequestBody Book book,@PathVariable("bookId") int bookId){
+        bookService.updateBook(book,bookId);
+        return book;
     }
 
 }
