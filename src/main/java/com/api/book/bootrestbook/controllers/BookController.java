bfbae1c98da80c58086f77dd6f0e3c64ce2328bd
SafaaC <safaaacherak@gmail.com>
2023-06-16 13:18:33 +0530

    getMapping

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -1,15 +1,27 @@
 package com.api.book.bootrestbook.controllers;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.book.bootrestbook.entities.Book;
+import com.api.book.bootrestbook.services.BookService;
 
 @RestController
 public class BookController {
 
-    @GetMapping(books)
-    public String getBook(){
-        return "this is testing book first" ;
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public List<Book> getBooks(){
+        return bookService.getAllBooks();
     }
     
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") int id){
+        return bookService.getBookById(id);
+    }
 }
