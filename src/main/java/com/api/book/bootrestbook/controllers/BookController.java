efab450db0b34492fb2dcac731da124d34319a93
SafaaC <safaaacherak@gmail.com>
2023-06-16 11:44:03 +0530

    annotaion changed

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -1,15 +1,13 @@
 package com.api.book.bootrestbook.controllers;
-
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
 
-    @RequestMapping(value="/books", method = RequestMethod.GET)
+    @GetMapping(books)
     public String getBook(){
         return "this is testing book first" ;
     }
