736770f9e66f2b0c07339185c6cd7b609f7d0d81
SafaaC <safaaacherak@gmail.com>
2023-06-16 14:46:03 +0530

    delete method

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.api.book.bootrestbook.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,20 +18,30 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
-
+    
+    //get all books handler
     @GetMapping("/books")
     public List<Book> getBooks(){
         return bookService.getAllBooks();
     }
     
+    //get single book handler
     @GetMapping("/books/{id}")
     public Book getBook(@PathVariable("id") int id){
         return bookService.getBookById(id);
     }
 
+    //post book handler
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book){
         Book b=bookService.addBook(book);
         return b;
     }
+
+    //delete book handler
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId){
+            bookService.deleteBook(bookId);
+    }
+
 }
