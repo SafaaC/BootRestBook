5b041e26747e282029edeaef9ccd3f22c5325221
SafaaC <safaaacherak@gmail.com>
2023-06-16 14:01:55 +0530

    unwanted duplicate lines cleared

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -1,19 +1,5 @@
 package com.api.book.bootrestbook.controllers;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BookController {
-
-    @GetMapping(books)
-    public String getBook(){
-        return "this is testing book first" ;
-    }
-    
-=======
+   
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +25,4 @@ public class BookController {
     public Book getBook(@PathVariable("id") int id){
         return bookService.getBookById(id);
     }
->>>>>>> bfbae1c98da80c58086f77dd6f0e3c64ce2328bd
 }
