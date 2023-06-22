e8b5152801f3a3f2592da87c1abc48b6ba7f0214
SafaaC <safaaacherak@gmail.com>
2023-06-22 12:29:26 +0530

    Serving file

--- src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.api.book.bootrestbook.helper.FileUploadHelper;
 
@@ -32,7 +33,9 @@ public class FileUploadController {
 
             Boolean f=fileUploadHelper.uploadFile(file);
             if(f){
-                return ResponseEntity.ok("File Uploaded successfully");
+                //return ResponseEntity.ok("File Uploaded successfully");
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(file.getOriginalFilename()).toUriString());
+                
             }
         }
         catch(Exception e){
